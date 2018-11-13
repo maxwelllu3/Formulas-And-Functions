286d37db547c0f28a301c87aa427ac2de104108c
maxwelllu3 <blu@lcs.on.ca>
2018-11-13 14:50:44 -0500

    Added "b" and a bunch of comments.

--- Formulas-And-Functions/main.swift
+++ Formulas-And-Functions/main.swift
@@ -1,14 +1,16 @@
-//
-//  main.swift
-//  Formulas-And-Functions
-//
-//  Formulas quick reference PDF obtained from Miami-Dade College:
-//  http://www.mdc.edu/main/images/COMMON_MATH_FORMULAS_tcm6-33520.pdf
-//
-
 import Foundation
 
 // Find the area of a square, using a function
-let a = 7.0
+let a = 7.0         // "a" is a constant (let)
 let areaOfSquare = forSquareWith(sideLength: a)
 print("The area of a square with side length \(a) is \(areaOfSquare)")
+
+
+// a = 8    // This cannot happen,"a" is a constant
+
+// Example of using a variable
+var b = 17.0
+b = 18.0
+// How to print a variable to the screen
+// \(b) ... this is called string interpolation
+print("the value of b is \(b)")
