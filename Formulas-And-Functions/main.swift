54e38f24f6ed75be9ab61793cf17041eb84cb649
Russell Gordon <mail@russellgordon.ca>
2018-11-13 12:26:45 -0500

    Initial Commit

---
+++ Formulas-And-Functions/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Formulas-And-Functions
+//
+//  Created by Gordon, Russell on 2018-11-13.
+//  Copyright © 2018 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
